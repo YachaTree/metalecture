989fb6b2bcd16d29052d3a1de6f758cf26a98531
JaeHyup <dnftksghkfkd@gmail.com>
2024-06-25 14:38:51 +0900

    jdkDynamicProxy

--- 01_spring-core/01_lecture-source/chap05-aop/src/main/java/com/i5/section01/aop/Application.java
+++ 01_spring-core/01_lecture-source/chap05-aop/src/main/java/com/i5/section01/aop/Application.java
@@ -1,5 +1,7 @@
 package com.i5.section01.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,6 +13,9 @@ public class Application {
 
         System.out.println("findAllMembers =========================");
         System.out.println(memberService.findMembers());
+        System.out.println("findMemberById =========================");
+        System.out.println(memberService.findMemberById(3L));
     }
+
 
 }
--- 01_spring-core/01_lecture-source/chap05-aop/src/main/java/com/i5/section01/aop/ContextConfiguration.java
+++ 01_spring-core/01_lecture-source/chap05-aop/src/main/java/com/i5/section01/aop/ContextConfiguration.java
@@ -1,8 +1,10 @@
 package com.i5.section01.aop;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true) //proxyTargetClass : ?
 public class ContextConfiguration {
 
 }
