e15e731cb101e6dd2437396b3cc2898e69f0d8bf
JaeHyup <dnftksghkfkd@gmail.com>
2024-06-28 09:38:00 +0900

    sql practice 완료

--- 02_MYSQL/02_practice/01_practice.sql
+++ 02_MYSQL/02_practice/01_practice.sql
@@ -18,3 +18,53 @@ WHERE
                     tb_department
             WHERE tb_department.DEPARTMENT_NO = '001'
     );
+
+SELECT
+        DEPARTMENT_NAME
+    ,   CATEGORY
+FROM
+        tb_department
+WHERE
+        CAPACITY >= 20 AND CAPACITY <=30;
+
+SELECT
+        PROFESSOR_NAME
+FROM
+        tb_professor
+WHERE
+        DEPARTMENT_NO IS NULL;
+
+SELECT
+        DEPARTMENT_NO
+FROM
+        tb_student
+WHERE
+        DEPARTMENT_NO IS NULL;
+
+SELECT
+        CLASS_NO
+FROM
+        tb_class
+WHERE
+        PREATTENDING_CLASS_NO IS NOT NULL;
+
+SELECT
+        CATEGORY
+FROM
+        tb_department
+GROUP BY
+        CATEGORY
+ORDER BY
+        CATEGORY ASC ;
+
+SELECT
+        STUDENT_NO
+     ,  STUDENT_NAME
+    ,   STUDENT_SSN
+FROM
+        tb_student
+WHERE
+        ABSENCE_YN = 'N' AND SUBSTR(ENTRANCE_DATE, 3,2) = '19' AND SUBSTR(STUDENT_ADDRESS,1,2) = '전주'
+ORDER BY
+        STUDENT_NAME ASC ;
+
